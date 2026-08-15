c63972514cedceaafc48c58c69fc509b94491c1f
Saliha Sadaqat <salihasatti876@gmail.com>
2026-08-15 23:34:46 +0500

    Connect frontend to deployed backend

--- Frontend/ecommerce-frontend/src/api/axios.js
+++ Frontend/ecommerce-frontend/src/api/axios.js
@@ -1,26 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL:
+    "https://saliha-s-beauty-full-stack-websites-seven.vercel.app/api",
+  withCredentials: true,
 });
-
-// Add JWT token automatically
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default API;
