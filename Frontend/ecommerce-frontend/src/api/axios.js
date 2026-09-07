6ea52527df490a575011ebe03521dad695546dc6
Saliha Sadaqat <salihasatti876@gmail.com>
2026-09-07 21:02:52 +0500

    Prepare frontend for Vercel deployment

--- Frontend/ecommerce-frontend/src/api/axios.js
+++ Frontend/ecommerce-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://saliha-s-beauty-website-d3bq.vercel.app/",
   withCredentials: true,
 });
 
